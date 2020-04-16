a9a3b998843623fafd82e61a256bbeca852bac2b
Paula Leite <paula.t.leite@icloud.com>
2020-04-16 14:23:17 -0300

    Request from url

---
+++ GIFiado/Controller/HolidayRequest.swift
@@ -0,0 +1,14 @@
+//
+//  HolidayRequest.swift
+//  GIFiado
+//
+//  Created by Paula Leite on 16/04/20.
+//  Copyright © 2020 Paula Leite. All rights reserved.
+//
+
+import Foundation
+
+struct HolidayRequest {
+    let resourceURL: URL
+    let API_KEY = "f6be48394ce929a7edfaffc108f4e5395dcdb6fe"
+}
