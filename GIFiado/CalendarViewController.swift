957ac4b62961aad1383613ac566b61d282e44c62
Paula Leite <paula.t.leite@icloud.com>
2020-04-16 11:52:25 -0300

    Added all of the vars and lets

--- GIFiado/CalendarViewController.swift
+++ GIFiado/CalendarViewController.swift
@@ -14,9 +14,23 @@ class CalendarViewController: UIViewController {
     
     @IBOutlet weak var monthLabel: UILabel!
     
-    let months = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"]
+    let months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
     
-    let weekDays = ["Segunda-feira","Terça-feira","Quarta-feira","Quinta-feira","Sexta-feira","Sábado","Domingo"]
+    let weekdays = ["Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado", "Domingo"]
+    
+    var daysInEachMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    
+    var currentMonth = String()
+    
+    var numberOfEmptyBoxes = Int()
+    
+    var nextNumberOfEmptyBoxes = Int()
+    
+    var previousNumberOfEmptyBoxes = 0
+    
+    var direction = 0
+    
+    var positionIndex = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
